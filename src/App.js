dd184d7db3f854d33ab7bd6e1812fb4d75bb6409
onias17 <oniasnephiisrael@gmail.com>
2020-11-30 10:22:47 -0800

    created logout button component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import react from 'react';
 import Navbar from './components/Navbar';
 import LoginButton from './components/LoginButton';
+import LogoutButton from './components/LogoutButton';
 
 function App() {
   return (
     <div className="App">
       <LoginButton />
+      <LogoutButton />
       {/* <Navbar /> */}
     </div>
   );
