092eb15f1d22ac05b06b0f32599d4674292fad47
onias17 <oniasnephiisrael@gmail.com>
2020-11-30 10:54:09 -0800

    added loading

--- src/App.js
+++ src/App.js
@@ -3,17 +3,20 @@ import Navbar from './components/Navbar';
 import LoginButton from './components/LoginButton';
 import LogoutButton from './components/LogoutButton';
 import Profile from './components/Profile';
+import { useAuth0 } from '@auth0/auth0-react';
 
 function App() {
+  const { isLoading } = useAuth0();
+
+  if (isLoading) return <div>Loading...</div>
+  
   return (
-    <div className="App">
       <>
         <LoginButton />
         <LogoutButton />
         <Profile />
         {/* <Navbar /> */}
       </>
-    </div>
   );
 }
 
