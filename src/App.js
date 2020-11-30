e57181444a913e1f85ba503552bcca1f53ba57ef
onias17 <oniasnephiisrael@gmail.com>
2020-11-30 10:29:41 -0800

    created profile component

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import react from 'react';
 import Navbar from './components/Navbar';
 import LoginButton from './components/LoginButton';
 import LogoutButton from './components/LogoutButton';
+import Profile from './components/Profile';
 
 function App() {
   return (
     <div className="App">
-      <LoginButton />
-      <LogoutButton />
-      {/* <Navbar /> */}
+      <>
+        <LoginButton />
+        <LogoutButton />
+        <Profile />
+        {/* <Navbar /> */}
+      </>
     </div>
   );
 }
